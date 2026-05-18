dd30d8c076c04ae8134e0711d4f5c6022bb2c0e6
sntyuri <y@professor.y>
2026-05-18 13:48:20 -0300

    criacao do AP

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 void setup() {
   //comunicacao serial definida no platformio.ini
@@ -12,10 +13,20 @@ void setup() {
   Serial.printf("[%lu] [SYSTEM] Initializing ESP32-S3 Core...\n", millis());
   Serial.printf("[%lu] [SYSTEM] Free Heap: %u bytes\n", millis(), ESP.getFreeHeap());
   Serial.printf("[%lu] [SYSTEM] Setup complete.\n", millis());
+
+  //definindo o esp como acess point
+  WiFi.mode(WIFI_AP);
+  
+  //Inicia a rede com nome "ESP32-Scan" e senha "senha123"
+  WiFi.softAP("ESP32-Scan", "senha123");
+  
+  // printando o endereço IP gerado no terminal pra poder acessar depois
+  Serial.printf("[%lu] [WIFI] Access Point Inicializado.\n", millis());
+  Serial.printf("[%lu] [WIFI] IP de controle: %s\n", millis(), WiFi.softAPIP().toString().c_str());
 }
 
 void loop() {
   //imprime o estado da memoria a cada 5 segundos para sabermos que ele nao travou (útil para detectar vazamentos futuros)
-  Serial.printf("[%lu] [SYSTEM] Standby. Free Heap: %u bytes\n", millis(), ESP.getFreeHeap());
+  Serial.printf("[%lu] [SYSTEM] Standby. Free Heap: %u bytes | Clients: %d\n", millis(), ESP.getFreeHeap(), WiFi.softAPgetStationNum());
   delay(5000);
 }

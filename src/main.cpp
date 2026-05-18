cd001065b05f37a1b8978a9fb08be26d4cdf917a
sntyuri <kirinoyuri@gmail.com>
2026-05-18 08:14:45 -0300

    feat: configuracao do src/main.cpp e validando serial nativa do esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //comunicacao serial definida no platformio.ini
+  Serial.begin(115200);
+  
+  //truque do esp32 s3 que aprendi num video do YT: sempre coloque um delay no início 
+  //pra dar tempo do seu PC (Linux) montar a porta /dev/ttyACM0 antes do chip comecar a enviar os logs do processo de boot.
+  //sem esse delay os logs são perdidos!
+  delay(3000); 
+  //formato padrao de logs: [timestamp] [modulo] mensagem
+  Serial.printf("[%lu] [SYSTEM] Initializing ESP32-S3 Core...\n", millis());
+  Serial.printf("[%lu] [SYSTEM] Free Heap: %u bytes\n", millis(), ESP.getFreeHeap());
+  Serial.printf("[%lu] [SYSTEM] Setup complete.\n", millis());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //imprime o estado da memoria a cada 5 segundos para sabermos que ele nao travou (útil para detectar vazamentos futuros)
+  Serial.printf("[%lu] [SYSTEM] Standby. Free Heap: %u bytes\n", millis(), ESP.getFreeHeap());
+  delay(5000);
 }
